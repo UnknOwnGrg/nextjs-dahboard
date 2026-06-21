97e8a11ea5839bbc23e1df92c9ff6bdeca49b939
UnknOwnGrg <ishakgrg12@gmail.com>
2026-06-21 19:26:21 +0545

    Error Handling

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -15,8 +15,6 @@ export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
-
-    console.log("Fetching revenue data...");
     await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
@@ -160,6 +158,7 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
+    console.log(invoice);
     return invoice[0];
   } catch (error) {
     console.error("Database Error:", error);
